70f079fc69112ea9db592bd32c8b7192ec885ff0
Akshansh Shrivastava <147258016+akshanshshrivastava09@users.noreply.github.com>
2025-01-24 17:06:42 +0000

    24/01/25 more components are added into the project and A protected layout is created

---
+++ Blog_App/src/Components/AuthLayout.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import useNavigate from 'react-router-dom' 
+
+export default function Protected({children , authentication =true}) {
+    const navigate = useNavigate()
+    const[loading ,setLoading] =useState(true)
+    const authStatus = useSelector(state=>state.auth.status)
+
+    useEffect(() => {
+        /*cleaned up code 
+        if(authentication && authStatus !== authentication){
+        navigate("/login")
+        }
+        else if(!authentication && authStatus !== authentication ){
+            navigate("/");
+        } 
+            */
+        /* simplified code 
+        if(authStatus !== authentication){
+        navigate(authentication?"/login":"/")
+        }
+        */
+      if(authentication && authStatus !== authentication){
+        navigate("/login")
+        }
+        else if(!authentication && authStatus !== authentication ){
+            navigate("/");
+        }
+        setLoading(false)
+    
+    }, [navigate,authStatus ,authentication])
+    
+  return loading? <h1>Loading...</h1> : <>{children}</>
+
+}
---
+++ Blog_App/src/Components/Select.jsx
@@ -0,0 +1,30 @@
+import {React ,useId} from 'react'
+
+
+function Select({
+    options,
+    label,
+    className = "",
+    ...props
+},ref) {
+    id =useId()
+  return (
+    <div className="w-full">
+        {label&& <label htmlFor={id} className=""></label>}
+        <select 
+        {...props}
+        id={id}
+        ref = {ref}
+        className={`px-3 py-2 rounded-lg bg-white text-black  outline-none focus:bg-grey-50 duration-500 border border-200-grey w-full ${className}`}
+        >
+           /*we will loop in options but we will check that options are existing or not otherwise our project will crash  */
+           /*options by default give array  */
+           {options?.map(option=>{
+            <option key={option} value={option}>{option}</option>
+           })}
+        </select>
+    </div>
+  )
+}
+
+export default React.forwardRef(Select);
